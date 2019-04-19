10c030043564a7006e7737f40b57f42b2098e968
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-18 22:37:14 -0300

    get current user location

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
-const Home = () => <span>Home</span>;
+class Home extends React.Component {
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(this.onLocationGranted, this.onLocationRejected);
+  }
+
+  onLocationGranted = ({ coords: { latitude, longitude } }) => console.log('permitiu', latitude, longitude);
+
+  onLocationRejected = () => console.log('n permitiu');
+
+  render() {
+    return <span>Home</span>;
+  }
+}
 
 export default Home;
