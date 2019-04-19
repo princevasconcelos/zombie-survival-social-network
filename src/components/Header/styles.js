cd07c1bef82e2cc6d81d2a41dfba23f3c6a689e1
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-19 07:37:22 -0300

    change Header to a header tag

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 import styles from '../../utils/constraints';
 
-const StyledHeader = styled.div`
+const StyledHeader = styled.header`
   display: flex;
   justify-content: center;
   align-items: center;
