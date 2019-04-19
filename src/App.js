8323b4fa58ff3eee296099ae3e4cc4fc0ec2864c
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-18 23:15:58 -0300

    add global styles to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import GlobalStyle from './global-styles';
+
 import Home from './pages/Home';
 import Profile from './pages/Profile';
 import Trade from './pages/Trade';
 
 const App = () => (
   <Router>
+    <GlobalStyle />
     <Route path="/" exact component={Home} />
     <Route path="/new" component={Profile} />
     <Route path="/trade" component={Trade} />
