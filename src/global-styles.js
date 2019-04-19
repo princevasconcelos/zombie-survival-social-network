8aacc40d2e5709018b0fa6f1294cb9efbc819b49
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-18 23:01:28 -0300

    add global styles

---
+++ src/global-styles.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:300|Spicy+Rice');
+
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html,
+  body,
+  #root {
+    height: 100%;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    font-size: 16px;
+  }
+`;
+
+export default GlobalStyle;
