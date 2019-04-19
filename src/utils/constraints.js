630230dc38244ea0edea573d46b147c81054bb6e
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-19 05:00:57 -0300

    add screens props and colors

--- src/utils/constraints.js
+++ src/utils/constraints.js
@@ -1,13 +1,19 @@
 const styles = {
   colors: {
-    line: '#dcdcdc',
     primary: '#612F74',
     black: '#3f3e3e',
     blacker: '#414143',
+    gray: '#dcdcdc',
     lightestGray: '#f2f2f2',
+    darkGray: '#a9a9a9',
   },
   sizes: {
     xlarge: '32px',
+    default: '16px',
+  },
+  screens: {
+    tablet: '800px',
+    desktop: '1200px',
   },
 };
 
