6a23407642c87af64169082f9207fa1da4f8af50
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-19 19:05:48 -0300

    add main style

--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -1,6 +1,11 @@
 import styled from 'styled-components';
 
 const StyledMain = styled.main`
+  display: flex;
+  margin: 0 auto;
+  justify-content: center;
+  max-width: 1200px;
+  padding: 16px 0;
   height: 100%;
 `;
 
