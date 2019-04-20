4b43c19348581913bb5751e8ea6a6c38c5bc257d
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-20 20:08:41 -0300

    decrease Account max width

--- src/pages/Account/styles.js
+++ src/pages/Account/styles.js
@@ -14,7 +14,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  max-width: 800px;
+  max-width: 600px;
   margin: 0 auto;
 
   padding: 16px 0;
