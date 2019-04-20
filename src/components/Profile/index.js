9825593fa85f1d0a7c712dfccbdd3ba8e64f0b8a
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-20 15:59:15 -0300

    add formik to Profile

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,5 +1,49 @@
 import React from 'react';
+import { Formik } from 'formik';
 
-const Profile = () => <span>Profile</span>;
+import Form from './styles';
+import Input from './Input';
+import Select from './Select';
+import Row from './Row';
+
+const Profile = () => (
+  <Formik
+    initialValues={{
+      name: '',
+      age: '',
+      genre: '',
+      items: [],
+      lonlat: {},
+    }}
+    onSubmit={(values) => {
+      console.log(values);
+    }}
+  >
+    {({ values, handleSubmit, handleChange }) => (
+      <Form onSubmit={handleSubmit}>
+        <Input
+          value={values.name}
+          onChange={handleChange}
+          label="Name"
+          type="text"
+          name="name"
+          id="name"
+        />
+        <Row>
+          <Input
+            value={values.age}
+            onChange={handleChange}
+            label="Age"
+            type="tel"
+            name="age"
+            id="age"
+          />
+          <Select />
+        </Row>
+        <button type="submit">Enviar</button>
+      </Form>
+    )}
+  </Formik>
+);
 
 export default Profile;
