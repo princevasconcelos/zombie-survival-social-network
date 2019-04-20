db39b7c378e226352f302fff82e27c0ec199fa1c
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-20 14:50:49 -0300

    add tertiary and darkGray color

--- src/utils/constraints.js
+++ src/utils/constraints.js
@@ -2,8 +2,10 @@ const styles = {
   colors: {
     primary: '#612F74',
     secondary: '#f6821f',
+    tertiary: '#1e90ff',
     black: '#3f3e3e',
     blacker: '#414143',
+    darkGray: '#a9a9a9',
     gray: '#dcdcdc',
     lightestGray: '#f2f2f2',
   },
