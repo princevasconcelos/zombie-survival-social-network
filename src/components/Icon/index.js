6e51baad1ddcd5f2b6927938d7f441cd4493032d
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-20 00:09:22 -0300

    remove unused color prop and add close icon

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import t from 'prop-types';
 
-import styles from '../../utils/constraints';
-
 import Search from './svgs/search';
+import Close from './svgs/close';
 
 const propTypes = {
   name: t.string.isRequired,
-  color: t.string,
 };
 
-const defaultProps = {
-  color: styles.colors.blacker,
+const Icon = ({ name }) => {
+  if (name === 'search') return <Search />;
+  if (name === 'close') return <Close />;
+  return <></>;
 };
-
-const Icon = ({ name, color }) => name === 'search' && <Search fill={color} />;
 
 Icon.propTypes = propTypes;
-Icon.defaultProps = defaultProps;
 
 export default Icon;
