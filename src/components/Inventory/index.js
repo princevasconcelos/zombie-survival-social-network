a33ea4f15a66b8ee18f284ee96da5f1a057a5277
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-21 00:26:46 -0300

    add Item to Inventory

--- src/components/Inventory/index.js
+++ src/components/Inventory/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 // import StyledInventory from './styles';
 import Box from '../Box';
+import Item from './Item';
 
 const Inventory = ({ items }) => (
-  <Box title="Inventory">
-    <span>Agua 13</span>
+  <Box title="Inventory" withBorder>
+    <Item name="Water" value="4" />
+    <Item name="Food" value="3" />
+    <Item name="Medication" value="1" />
+    <Item name="Ammunition" value="120" />
   </Box>
 );
 
