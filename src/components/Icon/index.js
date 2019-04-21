632fc311fd2054416faae1d6bfb44502f16ad878
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-20 22:14:02 -0300

    add Link to Icon

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import t from 'prop-types';
 
+import Link from '../Link';
+
 import Search from './svgs/search';
 import Close from './svgs/close';
+import Edit from './svgs/edit';
 
 const propTypes = {
   name: t.string.isRequired,
+  link: t.string,
 };
 
-const Icon = ({ name }) => {
+const defaultProps = {
+  link: '',
+};
+
+const getComponent = (name) => {
   if (name === 'search') return <Search />;
   if (name === 'close') return <Close />;
+  if (name === 'edit') return <Edit />;
   return <></>;
 };
 
+const Icon = ({ name, link }) => {
+  if (link) {
+    return <Link to={link}>{getComponent(name)}</Link>;
+  }
+  return getComponent(name);
+};
+
 Icon.propTypes = propTypes;
+Icon.defaultProps = defaultProps;
 
 export default Icon;
