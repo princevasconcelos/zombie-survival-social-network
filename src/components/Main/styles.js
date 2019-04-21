36f15b172b9e751e0ecd6a8649375358de43656d
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-21 00:55:07 -0300

    add flex column to Main

--- src/components/Main/styles.js
+++ src/components/Main/styles.js
@@ -2,6 +2,8 @@ import styled from 'styled-components';
 
 const StyledMain = styled.main`
   display: flex;
+  flex-direction: column;
+  align-items: center;
   margin: 0 auto;
   justify-content: center;
   max-width: 1200px;
