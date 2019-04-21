5344ae2f3ec15dde302da5bf63a4b06c93af2320
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-20 22:16:26 -0300

    remove mock Link

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 import Header from '../../components/Header';
-// import Main from '../../components/Main';
+import Main from '../../components/Main';
 
 class Home extends React.Component {
   componentDidMount() {
@@ -14,12 +13,10 @@ class Home extends React.Component {
   onLocationRejected = () => console.log('n permitiu');
 
   render() {
-    const userId = '32jdjdh2u2i2e22o3j';
     return (
       <>
         <Header />
-        {/* <Main /> */}
-        <Link to={`/survivor/${userId}`}>editar</Link>
+        <Main />
       </>
     );
   }
