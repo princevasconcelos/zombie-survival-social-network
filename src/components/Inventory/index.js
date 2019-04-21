05cb1b4436fcd076c8479db706c78d6189a2f1f6
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-20 22:16:01 -0300

    create Inventory

---
+++ src/components/Inventory/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// import StyledInventory from './styles';
+import Box from '../Box';
+
+const Inventory = ({ items }) => (
+  <Box title="Inventory">
+    <span>Agua 13</span>
+  </Box>
+);
+
+export default Inventory;
