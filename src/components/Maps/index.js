5d81998c206ba9ecb601a6776cdfa2ee0ae103c9
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-22 19:37:35 -0300

    add readOnly to Maps

--- src/components/Maps/index.js
+++ src/components/Maps/index.js
@@ -7,32 +7,42 @@ import Loading from '../Loading';
 import Container from './styles';
 
 const propTypes = {
-  onReady: t.func.isRequired,
+  onReady: t.func,
   google: t.shape({
     maps: t.object,
   }).isRequired,
   markers: t.arrayOf(t.object),
   onMarkerDragEnd: t.func,
   onMapClick: t.func,
+  readOnly: t.bool,
 };
 
 const defaultProps = {
+  onReady: () => {},
   markers: [],
   onMarkerDragEnd: () => {},
   onMapClick: () => {},
+  readOnly: false,
 };
 
 const Maps = ({
-  onReady, onMarkerDragEnd, google, markers, onMapClick,
+  onReady, onMarkerDragEnd, google, markers, onMapClick, readOnly,
 }) => (
   <Container>
-    <Map google={google} zoom={14} onClick={onMapClick} onReady={onReady} center={markers[0]}>
+    <Map
+      google={google}
+      zoom={14}
+      clicable={!readOnly}
+      onClick={onMapClick}
+      onReady={onReady}
+      center={markers[0]}
+    >
       {markers.length > 0
         && markers.map(mark => (
           <Marker
-            key={JSON.stringify(mark)}
+            key={JSON.stringify(mark).concat(Math.random())}
             title="The marker`s title will appear as a tooltip."
-            draggable
+            draggable={!readOnly}
             name="SOMA"
             onDragend={onMarkerDragEnd}
             position={{ lat: mark.lat, lng: mark.lng }}
