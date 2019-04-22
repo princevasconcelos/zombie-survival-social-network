4849c1452869ad928f395d746b0aa912a2ae1b88
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-22 04:03:47 -0300

    refactor Home to stateless

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,23 +3,11 @@ import React from 'react';
 import Header from '../../components/Header';
 import Main from '../../components/Main';
 
-class Home extends React.Component {
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(this.onLocationGranted, this.onLocationRejected);
-  }
-
-  onLocationGranted = ({ coords: { latitude, longitude } }) => console.log('permitiu', latitude, longitude);
-
-  onLocationRejected = () => console.log('n permitiu');
-
-  render() {
-    return (
-      <>
-        <Header />
-        <Main />
-      </>
-    );
-  }
-}
+const Home = () => (
+  <>
+    <Header />
+    <Main />
+  </>
+);
 
 export default Home;
