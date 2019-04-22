54768e8e7628e2dbe058cafdb3a509e50aadd8d4
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-22 04:02:30 -0300

    add getUserLocation, onMarkerChangeHandler and onMapClickHandler

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -7,10 +7,12 @@ import Profile from '../../components/Profile';
 import Icon from '../../components/Icon';
 import Button from '../../components/Button';
 import Maps from '../../components/Maps';
+import Box from '../../components/Box';
 
 class Account extends React.Component {
   state = {
     id: '',
+    userLocation: [],
   };
 
   static propTypes = {
@@ -32,13 +34,33 @@ class Account extends React.Component {
     if (id) this.setState({ id });
   }
 
+  getUserLocation = () => {
+    navigator.geolocation.getCurrentPosition(this.onLocationGranted, this.onLocationRejected);
+  };
+
+  onLocationGranted = ({ coords: { latitude, longitude } }) => this.setState({ userLocation: [{ lat: latitude, lng: longitude }] });
+
+  onLocationRejected = () => console.log('n permitiu');
+
+  onMarkerChangeHandler = (mapProps, map, coords) => {
+    const { latLng } = coords;
+    this.setState({ userLocation: [{ lat: latLng.lat(), lng: latLng.lng() }] });
+  };
+
+  onMapClickHandler = (mapProps, map, coords) => {
+    const { userLocation } = this.state;
+    if (userLocation.length > 0) return;
+    const { latLng } = coords;
+    this.setState({ userLocation: [{ lat: latLng.lat(), lng: latLng.lng() }] });
+  };
+
   handleClose = () => {
     const { history } = this.props;
     history.goBack();
   };
 
   render() {
-    const { id } = this.state;
+    const { id, userLocation } = this.state;
     return (
       <Container>
         <Header>
@@ -47,11 +69,18 @@ class Account extends React.Component {
             <Icon name="close" />
           </Floating>
         </Header>
-        <Profile />
-        <Maps />
-        <Button type="submit" form="profile-form">
+        <Button color="blue" type="submit" form="profile-form">
           Save
         </Button>
+        <Profile />
+        <Box title="Select your current location" withBorder>
+          <Maps
+            onReady={this.getUserLocation}
+            onMarkerDragEnd={this.onMarkerChangeHandler}
+            markers={userLocation}
+            onMapClick={this.onMapClickHandler}
+          />
+        </Box>
       </Container>
     );
   }
