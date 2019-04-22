017b64b35de8031edfbb1333d06a80df5937c514
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-22 06:24:51 -0300

    create postSurvivor

--- src/services/api.js
+++ src/services/api.js
@@ -1,26 +1,42 @@
 const baseUrl = 'http://zssn-backend-example.herokuapp.com/api';
 
-const init = {
+const getHeaders = {
   headers: new Headers({
     Accept: 'application/json',
   }),
 };
 
-const get = url => fetch(url, init)
+const postHeaders = {
+  headers: new Headers({
+    Accept: 'application/json',
+    ContentType: 'application/x-www-form-urlencoded',
+  }),
+};
+
+const get = url => fetch(url, getHeaders)
   .then(response => response.json())
   .catch(error => error);
 
-const getAll = type => fetch(`${baseUrl}/${type}.json`, init)
+const getAll = type => fetch(`${baseUrl}/${type}.json`, getHeaders)
   .then(response => response.json())
   .catch(error => error);
 
 const getReports = () => getAll('report');
 const getSurvivors = () => getAll('people');
 
+const postSurvivor = formData => fetch(`${baseUrl}/people.json`, {
+  method: 'post',
+  headers: postHeaders,
+  body: formData,
+})
+  .then(response => console.log(response))
+  .catch(error => error);
+
 const API = {
   get,
   getReports,
   getSurvivors,
+  postSurvivor,
 };
 
 export default API;
