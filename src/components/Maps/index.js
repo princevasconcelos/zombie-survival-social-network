16c3112814d48473ef1ade928c75e84bb61927cc
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-22 04:01:14 -0300

    add map click and drag functionality

--- src/components/Maps/index.js
+++ src/components/Maps/index.js
@@ -1,5 +1,51 @@
 import React from 'react';
+import t from 'prop-types';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
-const Maps = () => <span>maps</span>;
+import Loading from '../Loading';
 
-export default Maps;
+import Container from './styles';
+
+const propTypes = {
+  onReady: t.func.isRequired,
+  google: t.shape({
+    maps: t.object,
+  }).isRequired,
+  markers: t.arrayOf(t.object),
+  onMarkerDragEnd: t.func,
+  onMapClick: t.func,
+};
+
+const defaultProps = {
+  markers: [],
+  onMarkerDragEnd: () => {},
+  onMapClick: () => {},
+};
+
+const Maps = ({
+  onReady, onMarkerDragEnd, google, markers, onMapClick,
+}) => (
+  <Container>
+    <Map google={google} zoom={14} onClick={onMapClick} onReady={onReady} center={markers[0]}>
+      {markers.length > 0
+        && markers.map(mark => (
+          <Marker
+            key={JSON.stringify(mark)}
+            title="The marker`s title will appear as a tooltip."
+            draggable
+            name="SOMA"
+            onDragend={onMarkerDragEnd}
+            position={{ lat: mark.lat, lng: mark.lng }}
+          />
+        ))}
+    </Map>
+  </Container>
+);
+
+Maps.propTypes = propTypes;
+Maps.defaultProps = defaultProps;
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyDlAR3FiEF2YJ-JYahQLh1Xh9gRLUUqijM',
+  LoadingContainer: Loading,
+})(Maps);
---
+++ src/components/Maps/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 16px;
+  height: 400px;
+`;
+
+export default Container;
