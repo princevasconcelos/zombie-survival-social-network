61be738e3e73316fea088be22b7ef5a0fab6ccb7
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-23 03:58:47 -0300

    setup redux

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+import store from './stores';
 import GlobalStyle from './global-styles';
 
 import Home from './pages/Home';
@@ -8,13 +10,15 @@ import Account from './pages/Account';
 import Trade from './pages/Trade';
 
 const App = () => (
-  <Router>
-    <GlobalStyle />
-    <Route path="/" exact component={Home} />
-    <Route path="/register" component={Account} />
-    <Route path="/survivor/:id" component={Account} />
-    <Route path="/trade" component={Trade} />
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <GlobalStyle />
+      <Route path="/" exact component={Home} />
+      <Route path="/register" component={Account} />
+      <Route path="/survivor/:id" component={Account} />
+      <Route path="/trade" component={Trade} />
+    </Router>
+  </Provider>
 );
 
 export default App;
