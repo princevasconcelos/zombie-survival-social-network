73827ad25f3c17a458a1dca519ef864ddce82bb8
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-24 02:37:13 -0300

    add formik to Trade

--- src/pages/Trade/index.js
+++ src/pages/Trade/index.js
@@ -1,5 +1,76 @@
 import React from 'react';
+import t from 'prop-types';
+import { connect } from 'react-redux';
+import { Formik } from 'formik';
 
-const Trade = () => <span>Trade</span>;
+import { Container, Score } from './styles';
 
-export default Trade;
+import Inventory from '../../components/Inventory';
+
+const arturItems = [
+  {
+    quantity: 0,
+  },
+  {
+    quantity: 0,
+  },
+  {
+    quantity: 0,
+  },
+  {
+    quantity: 0,
+  },
+];
+
+const propTypes = {
+  user: t.shape({
+    data: t.shape({
+      items: t.arrayOf(t.object),
+    }),
+  }).isRequired,
+};
+
+const Trade = ({
+  user: {
+    data: { items },
+  },
+}) => (
+  <Formik
+    initialValues={{
+      offerItems: items,
+      desiredItems: arturItems,
+    }}
+  >
+    {({ values, handleChange, handleSubmit }) => (
+      <Container>
+        <div style={{ width: '100%' }}>
+          <Inventory items={values.offerItems} onChange={handleChange} boxTitle="Your items" />
+        </div>
+
+        <Score>
+          <span>POINTS: 13</span>
+          <span>POINTS: 12</span>
+        </Score>
+
+        {/* <div style={{ width: '100%' }}>
+          <Inventory
+            inverted
+            items={values.desiredItems}
+            onChange={handleChange}
+            boxTitle="Artur items"
+          />
+        </div> */}
+      </Container>
+    )}
+  </Formik>
+);
+
+Trade.propTypes = propTypes;
+
+const mapStateToProsp = ({ user }) => ({
+  user,
+});
+
+export default connect(mapStateToProsp)(Trade);
+
+// 1 unico dispatch para atualizar meus items quando salvar
---
+++ src/pages/Trade/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+
+const Score = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
+
+export { Container, Score };
