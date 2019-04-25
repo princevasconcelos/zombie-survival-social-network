ec0571d0b35a96916e09f5e4cb3b82a472fc0efb
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-24 22:03:25 -0300

    add items from redux and fix layout

--- src/pages/Trade/index.js
+++ src/pages/Trade/index.js
@@ -2,79 +2,158 @@ import React from 'react';
 import t from 'prop-types';
 import { connect } from 'react-redux';
 import { Formik } from 'formik';
+import { withRouter } from 'react-router-dom';
 
-import { Container, Score } from './styles';
+import API from '../../services/api';
+
+import {
+  Container,
+  Score,
+  Text,
+  Scoreboard,
+  Wrapper,
+  Header,
+  Floating,
+  Title,
+  InputWrapper,
+  ItemInput,
+} from './styles';
 
 import Inventory from '../../components/Inventory';
+import Icon from '../../components/Icon';
+import Loading from '../../components/Loading';
 
-const arturItems = [
-  {
-    quantity: 0,
-  },
-  {
-    quantity: 0,
-  },
-  {
-    quantity: 0,
-  },
-  {
-    quantity: 0,
-  },
-];
+class Trade extends React.Component {
+  state = {
+    name: '',
+    offerItems: [],
+    desiredItems: [],
+  };
 
-const propTypes = {
-  user: t.shape({
-    data: t.shape({
-      items: t.arrayOf(t.object),
-    }),
-  }).isRequired,
-};
+  static propTypes = {
+    user: t.shape({
+      data: t.shape({
+        items: t.arrayOf(t.object),
+      }),
+    }).isRequired,
+    survivors: t.shape({
+      data: t.array,
+    }).isRequired,
 
-const Trade = ({
-  user: {
-    data: { items },
-  },
-}) => (
-  <Formik
-    initialValues={{
+    location: t.shape({
+      pathname: t.string,
+    }).isRequired,
+    history: t.shape({
+      history: t.func,
+    }).isRequired,
+  };
+
+  async componentDidMount() {
+    const {
+      user: {
+        data: { items },
+      },
+      location: { pathname },
+    } = this.props;
+
+    const [name, id] = pathname
+      .split('/')
+      .slice(-1)[0]
+      .split('_');
+
+    const response = await API.getItems(id);
+
+    console.log(items);
+    this.setState({
+      name,
       offerItems: items,
-      desiredItems: arturItems,
-    }}
-  >
-    {({ values, handleChange }) => (
-      <Container>
-        <div style={{ width: '100%' }}>
-          <Inventory
-            items={values.offerItems}
-            onChange={handleChange}
-            forObject="offerItems"
-            boxTitle="Your items"
-          />
-        </div>
+      desiredItems: response,
+    });
+  }
 
-        <Score>
-          <span>POINTS: 13</span>
-          <span>POINTS: 12</span>
-        </Score>
+  handleClose = () => {
+    const { history } = this.props;
+    history.goBack();
+  };
 
-        <div style={{ width: '100%' }}>
-          <Inventory
-            inverted
-            items={values.desiredItems}
-            onChange={handleChange}
-            forObject="desiredItems"
-            boxTitle="Artur items"
-          />
-        </div>
-      </Container>
-    )}
-  </Formik>
-);
+  getPoints = items => items
+    .filter(e => !!e.quantity)
+    .reduce((prev, curr) => prev + +curr.quantity * curr.item.points, 0);
 
-Trade.propTypes = propTypes;
+  render() {
+    const {
+      user: { loading },
+    } = this.props; // remover items
 
-const mapStateToProsp = ({ user }) => ({
+    const { name, desiredItems, offerItems } = this.state;
+
+    if (!name) return <Loading />;
+    return (
+      <Formik
+        initialValues={{
+          offerItems,
+          desiredItems,
+        }}
+      >
+        {({ values, handleChange }) => (
+          <>
+            {/* <Header>
+              <Title>Trade</Title>
+              <Floating onClick={this.handleClose} isLoading={loading}>
+                <Icon name="close" />
+              </Floating>
+            </Header> */}
+            <Container>
+              <Wrapper>
+                <Inventory
+                  readOnly
+                  items={values.offerItems}
+                  onChange={handleChange}
+                  forObject="offerItems"
+                  boxTitle="Your items"
+                />
+                <InputWrapper>
+                  <ItemInput type="text" />
+                  <ItemInput type="text" />
+                  <ItemInput type="text" />
+                  <ItemInput type="text" />
+                </InputWrapper>
+              </Wrapper>
+
+              <Scoreboard>
+                <Score>{this.getPoints(values.offerItems)}</Score>
+                <Text>points</Text>
+                <Score>{this.getPoints(values.desiredItems)}</Score>
+              </Scoreboard>
+
+              <Wrapper>
+                <InputWrapper>
+                  <ItemInput type="text" />
+                  <ItemInput type="text" />
+                  <ItemInput type="text" />
+                  <ItemInput type="text" />
+                </InputWrapper>
+
+                <Inventory
+                  readOnly
+                  inverted
+                  items={values.desiredItems}
+                  onChange={handleChange}
+                  forObject="desiredItems"
+                  boxTitle={`${name} items`}
+                />
+              </Wrapper>
+            </Container>
+          </>
+        )}
+      </Formik>
+    );
+  }
+}
+
+const mapStateToProps = ({ user, survivors }) => ({
   user,
+  survivors,
 });
 
-export default connect(mapStateToProsp)(Trade);
+export default withRouter(connect(mapStateToProps)(Trade));
--- src/pages/Trade/styles.js
+++ src/pages/Trade/styles.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import styles from '../../utils/constraints';
+
 const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -9,12 +11,101 @@ const Container = styled.div`
   height: 100%;
 `;
 
-const Score = styled.div`
+const Score = styled.span`
   display: flex;
-  flex-direction: column;
-  justify-content: center;
   align-items: center;
-  background-color: red;
+  justify-content: center;
+  width: 100px;
+  height: 100px;
+  font-size: ${styles.sizes.xxlarge};
+  color: ${styles.colors.black};
+  box-shadow: 3px 3px 6px ${styles.colors.darkGray};
+  margin: 16px;
+  font-weight: bold;
 `;
 
-export { Container, Score };
+const Text = styled.span`
+  text-align: center;
+  text-transform: uppercase;
+  font-size: ${styles.sizes.xdefault};
+  letter-spacing: 2px;
+  font-weight: bold;
+  color: gray;
+`;
+
+const Scoreboard = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 100%;
+
+  @media (min-width: ${styles.screens.desktop}) {
+    max-width: 400px;
+  }
+`;
+
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+  height: 50px;
+  font-size: ${styles.sizes.large};
+  color: ${styles.colors.black};
+  margin-top: 8px;
+`;
+
+const Floating = styled.div`
+  display: ${({ isLoading }) => (isLoading ? 'none' : 'flex')};
+  cursor: pointer;
+  opacity: 0.3;
+  width: 32px;
+  height: 32px;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  width: 100%;
+  margin: 8px 0;
+
+  @media (min-width: ${styles.screens.largePhone}) {
+    max-width: 400px;
+    justify-content: space-between;
+  }
+
+  @media (min-width: ${styles.screens.desktop}) {
+    width: 100%;
+    justify-content: space-evenly;
+  }
+`;
+
+const ItemInput = styled.input`
+  width: 70px;
+  height: 50px;
+`;
+
+const Title = styled.span`
+  text-align: center;
+  padding: 0 60px;
+`;
+
+export {
+  Container,
+  Score,
+  Text,
+  Scoreboard,
+  Wrapper,
+  Header,
+  Floating,
+  Title,
+  InputWrapper,
+  ItemInput,
+};
