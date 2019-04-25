8e34a4cf562a8b0fbad3d912841e1b797b8482e0
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-25 02:42:46 -0300

    find inventory item index on array

--- src/components/Inventory/index.js
+++ src/components/Inventory/index.js
@@ -25,50 +25,57 @@ const defaultProps = {
 
 const Inventory = ({
   items, onChange, onBlur, readOnly, boxTitle, inverted, forObject,
-}) => (
-  <Box title={boxTitle} withBorder inverted={inverted}>
-    <Item
-      name="Water"
-      color="Water"
-      forObject={forObject}
-      position={0}
-      value={(items[0] || []).quantity}
-      handleChange={onChange}
-      onBlur={onBlur}
-      readOnly={readOnly}
-    />
-    <Item
-      name="Food"
-      color="Food"
-      forObject={forObject}
-      position={1}
-      value={(items[1] || []).quantity}
-      handleChange={onChange}
-      onBlur={onBlur}
-      readOnly={readOnly}
-    />
-    <Item
-      name="Medication"
-      color="Medication"
-      forObject={forObject}
-      position={2}
-      value={(items[2] || []).quantity}
-      handleChange={onChange}
-      onBlur={onBlur}
-      readOnly={readOnly}
-    />
-    <Item
-      name="Ammunition"
-      color="Ammunition"
-      forObject={forObject}
-      position={3}
-      value={(items[3] || []).quantity}
-      onBlur={onBlur}
-      handleChange={onChange}
-      readOnly={readOnly}
-    />
-  </Box>
-);
+}) => {
+  const waterIndex = items.findIndex(e => e.item.name === 'Water');
+  const foodIndex = items.findIndex(e => e.item.name === 'Food');
+  const medicationIndex = items.findIndex(e => e.item.name === 'Medication');
+  const ammunitionIndex = items.findIndex(e => e.item.name === 'Ammunition');
+
+  return (
+    <Box title={boxTitle} withBorder inverted={inverted}>
+      <Item
+        name="Water"
+        color="Water"
+        forObject={forObject}
+        position={waterIndex}
+        value={(items[waterIndex] || []).quantity}
+        handleChange={onChange}
+        onBlur={onBlur}
+        readOnly={readOnly}
+      />
+      <Item
+        name="Food"
+        color="Food"
+        forObject={forObject}
+        position={foodIndex}
+        value={(items[foodIndex] || []).quantity}
+        handleChange={onChange}
+        onBlur={onBlur}
+        readOnly={readOnly}
+      />
+      <Item
+        name="Medication"
+        color="Medication"
+        forObject={forObject}
+        position={medicationIndex}
+        value={(items[medicationIndex] || []).quantity}
+        handleChange={onChange}
+        onBlur={onBlur}
+        readOnly={readOnly}
+      />
+      <Item
+        name="Ammunition"
+        color="Ammunition"
+        forObject={forObject}
+        position={ammunitionIndex}
+        value={(items[ammunitionIndex] || []).quantity}
+        onBlur={onBlur}
+        handleChange={onChange}
+        readOnly={readOnly}
+      />
+    </Box>
+  );
+};
 
 Inventory.propTypes = propTypes;
 Inventory.defaultProps = defaultProps;
