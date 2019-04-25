e3046243dd9509db72a73981694d962fc18e27d8
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-25 12:08:34 -0300

    create storeQueryMiddleware to fetch with delay

---
+++ src/stores/sagas/query.js
@@ -0,0 +1,14 @@
+import { put, delay, call } from 'redux-saga/effects';
+
+import API from '../../services/api';
+
+import { storeQuery, discartQuery } from '../reducers/query';
+
+export default function* storeQueryMiddleware({ payload: { data, query } }) {
+  yield delay(1000);
+  const match = data.find(e => e.name === query);
+  if (!match) return yield put(discartQuery());
+  const id = match.location.split('/').slice(-1);
+  const result = yield call(API.fetchSurvivor, id);
+  return yield put(storeQuery(result));
+}
