dd5b5a7696c2fca0c9e8d75a0fdc6e64c2806ec1
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-04-25 12:07:53 -0300

    create search reducer for saga

--- src/stores/reducers/query.js
+++ src/stores/reducers/query.js
@@ -1,17 +1,40 @@
-const STORE_QUERY = 'STORE_QUERY';
+export const CHANGE_QUERY = 'CHANGE_QUERY';
 
-const INITIAL_STATE = '';
+const REQUEST_QUERY_SUCCESS = 'REQUEST_QUERY_SUCCESS';
+const REQUEST_QUERY_FAILED = 'REQUEST_QUERY_FAILED';
+
+const INITIAL_STATE = {
+  name: '',
+  loading: false,
+  error: false,
+};
+
+export const saveQuery = payload => ({
+  type: CHANGE_QUERY,
+  payload,
+});
 
 export const storeQuery = payload => ({
-  type: STORE_QUERY,
+  type: REQUEST_QUERY_SUCCESS,
   payload,
+});
+
+export const discartQuery = () => ({
+  type: REQUEST_QUERY_FAILED,
 });
 
 const queryReducer = (state = INITIAL_STATE, action) => {
   const { payload, type } = action;
   switch (type) {
-    case STORE_QUERY:
-      return payload;
+    case CHANGE_QUERY:
+      return {
+        ...state,
+        loading: true,
+      };
+    case REQUEST_QUERY_SUCCESS:
+      return { ...state, loading: false, ...payload };
+    case REQUEST_QUERY_FAILED:
+      return { ...state, error: true, loading: false };
     default:
       return state;
   }
