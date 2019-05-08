722b8d9d161ea011d03daec7cf18b5844e5818d9
Prince Vasconcelos <jpqvasconcelos@gmail.com>
2019-05-08 14:49:23 -0300

    add reducer to search test

--- src/__tests__/components/Search.test.js
+++ src/__tests__/components/Search.test.js
@@ -1,20 +1,27 @@
 import React from 'react';
-import { render, fireEvent } from 'react-testing-library';
+import { fireEvent, render } from 'react-testing-library';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+import rootReducer from '../../stores/reducers';
 
 import Search from '../../components/Search';
 
 describe('<Search />', () => {
-  test('typing should change inputs value', () => {
-    const { getByTestId } = render(<Search />);
+  const renderWithRedux = (
+    ui,
+    { initialState, store = createStore(rootReducer, initialState) } = {},
+  ) => ({
+    ...render(<Provider store={store}>{ui}</Provider>),
+    store,
+  });
 
+  it('should change inputs value when user input', () => {
+    const { getByTestId } = renderWithRedux(<Search />);
     const searchInput = getByTestId(/search/i);
-
     const query = 'nirvana';
-
     expect(searchInput.value).toEqual('');
-
     fireEvent.change(searchInput, { target: { value: query } });
-
     expect(searchInput.value).toEqual(query);
   });
 });
